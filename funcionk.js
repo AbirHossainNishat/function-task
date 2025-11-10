306463aae1e9e0a2cab0d355bc60bd9c6e9dab8a
AbirHossainNishat <abirnishat66@gmail.com>
2025-11-10 23:32:37 +0600

    last commit

--- funcionk.js
+++ funcionk.js
@@ -5,3 +5,16 @@ function transformNumber(n) {
     return n * 2;
   }
 }
+
+
+function count_zero(binaryString) {
+  let count = 0;
+
+  for (let i = 0; i < binaryString.length; i++) {
+    if (binaryString[i] === '0') {
+      count++;
+    }
+  }
+
+  return count;
+}
