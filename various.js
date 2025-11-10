37737e073c81a46aa5982038e9175835e6925c0c
AbirHossainNishat <abirnishat66@gmail.com>
2025-11-10 23:25:52 +0600

    fdjfjd

--- various.js
+++ various.js
@@ -1,19 +1,12 @@
-//  function sum(number1,number2,number3,number4){
+ 
+ 
+function make_avg(arr, size) {
+  if (size === 0) return 0; // avoid division by zero
 
-//   const som = number1 +number2+number3 +number4 ;
-//   console.log(som)
-
-
-
-//  }
-//  sum(3,4,5,35);
-
-
-
-function transformNumber(n) {
-  if (n % 2 === 0) { // even
-    return n / 2;
-  } else {            // odd
-    return n * 2;
+  let sum = 0;
+  for (let i = 0; i < size; i++) {
+    sum += arr[i];
   }
+
+  return sum / size;
 }
