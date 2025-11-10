b06773020904263a4e0d63da6820d1ca558e2d10
AbirHossainNishat <abirnishat66@gmail.com>
2025-11-10 23:19:59 +0600

    enything

--- various.js
+++ various.js
@@ -1,10 +1,19 @@
- function sum(number1,number2,number3,number4){
+//  function sum(number1,number2,number3,number4){
 
-  const som = number1 +number2+number3 +number4 ;
-  console.log(som)
+//   const som = number1 +number2+number3 +number4 ;
+//   console.log(som)
 
 
 
- }
- sum(3,4,5,35);
- 
+//  }
+//  sum(3,4,5,35);
+
+
+
+function transformNumber(n) {
+  if (n % 2 === 0) { // even
+    return n / 2;
+  } else {            // odd
+    return n * 2;
+  }
+}
